16966a7b8448a2d09f8cbd6f5e6dd0b9b144ec56
Alina-Pishak <alinapishak@icloud.com>
2024-01-12 01:19:37 +0200

    update upload file

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -13,19 +13,16 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
-    // Determine the folder based on file properties or request data
     let folder;
     if (file.fieldname === "avatar") {
       folder = "avatars";
-    } else if (file.fieldname === "documents") {
-      folder = "documents";
     } else {
       folder = "misc";
     }
     return {
       folder: folder,
-      allowed_formats: ["jpg", "png"], // Adjust the allowed formats as needed
-      public_id: file.originalname, // Use original filename as the public ID
+      allowed_formats: ["jpg", "png"],
+      public_id: req.user.id,
       transformation: [
         { width: 350, height: 350 },
         { width: 700, height: 700 },
