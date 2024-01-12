4111d876e77d0d78fcfdc4a0ac76a6efb0d769aa
IgorMushk <igor.mushketik@gmail.com>
2024-01-12 22:44:48 +0200

    del comment

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-//const handleMongooseError = require("../middlewares/handleMongooseError");// !!!
 const handleMongooseError = require("../helpers/handleMongooseError");
 
 const emailRegexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
