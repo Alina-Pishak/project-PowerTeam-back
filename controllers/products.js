47a051fdd68788f1fb8417580149a2693341eab2
liussi <125599487+liussi@users.noreply.github.com>
2024-01-13 15:15:15 +0200

    creation of the products and controllers section

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,24 @@
 const { Product } = require("../models/product");
-
 const { ctrlWrapper } = require("../helpers");
+const fs = require("fs");
+const path = require("path");
 
 const listProducts = async (req, res) => {
-     const result = await Product.find();
-     res.json(result);
+  const categoryPath = path.resolve(
+    __dirname,
+    "../products/productsCategories.json"
+  );
+
+  const categoriesData = fs.readFileSync(categoryPath, "utf8");
+  const categories = JSON.parse(categoriesData);
+
+  res.json(categories);
 };
 
 
 const listFilterProducts = async (req, res) => {
-  const { title, category, groupBloodNotAllowed } = req.user;
+  const { title, category } = req.query;
+  const { groupBloodNotAllowed } = req.user;
 
   const searchConditions = {};
 
