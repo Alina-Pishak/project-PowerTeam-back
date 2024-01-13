8ee75bd5ff27dc17689855671643018ddfd1ac7a
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-01-13 11:03:27 +0200

    controller added

---
+++ controllers/exercises.js
@@ -0,0 +1,37 @@
+const { ctrlWrapper, HttpError } = require("../helpers");
+const Exercise = require("../models/exercise");
+const Filters = require("../models/filter");
+
+const getAllExercises = async (req, res) => {
+  const result = await Exercise.find();
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const getAllByCategory = async (req, res) => {
+  const { category } = req.params;
+  const arrayCategories = ["bodyparts", "muscles", "equipment"];
+  if (!arrayCategories.includes(category)) {
+    throw HttpError(400, "Bad request");
+  }
+  const filterCategory = {
+    bodyparts: "Body parts",
+    muscles: "Muscles",
+    equipment: "Equipment",
+  };
+  const result = await ExercisesFilter.find({
+    filter: filterCategory[category],
+  });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllExercises: ctrlWrapper(getAllExercises),
+  getAllByCategory: ctrlWrapper(getAllByCategory),
+};
