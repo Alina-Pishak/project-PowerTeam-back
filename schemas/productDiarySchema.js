af6e8180a3364e4be079b398ccfe776339df71b3
Annacheer <125766262+Annacheer@users.noreply.github.com>
2024-01-13 12:22:55 -0500

    joi added

--- schemas/productDiarySchema.js
+++ schemas/productDiarySchema.js
@@ -4,8 +4,7 @@ Joi.extend(require("@joi/date"));
 const productSchema = Joi.object({
   productId: Joi.string().required(),
   date: Joi.date().format("DD/MM/YYYY").required().messages({
-    "any.required":
-      "The date field is required. Please provide a valid date in DD/MM/YYYY format.",
+    "any.required": "The date field is required.",
     "date.format": "Invalid date format. Please use DD/MM/YYYY format.",
   }),
   amount: Joi.number().min(1).required(),
