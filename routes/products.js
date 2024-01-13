85b18be8e1a6eaa67da21bf1842368dd6006d46f
liussi <125599487+liussi@users.noreply.github.com>
2024-01-13 16:42:49 +0200

    fix router products

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const ctrl = require("../controllers/products");
 const { authenticate } = require("../middlewares/index");
 const router = express.Router();
 
-router.get("/", authenticate,ctrl.listProducts);
-router.get("/recommend", authenticate, ctrl.listFilterProducts);
+router.get("/categories", authenticate, ctrl.listProducts);
+router.get("/", authenticate, ctrl.listFilterProducts);
 
 module.exports = router;
