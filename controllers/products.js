ec8634c30a96f9a287a9f50e58460bd63977568f
liussi <125599487+liussi@users.noreply.github.com>
2024-01-13 15:50:23 +0200

    fix import in controllers product

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const { Product } = require("../models/product");
+const  Product  = require("../models/product");
 const { ctrlWrapper } = require("../helpers");
 const fs = require("fs");
 const path = require("path");
