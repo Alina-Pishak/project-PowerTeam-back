b545ff8400b149d849373530079e0f16d46bc35f
Alina-Pishak <alinapishak@icloud.com>
2024-01-14 23:03:50 +0200

    add pattern helper

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 const authRouter = require("./routes/auth");
 const exercisesRouter = require("./routes/exercises");
 const productRouter = require("./routes/products");
-const statisticRoute = require("./routes/statistic")
-const diaryExercises = require("./routes/dairyExercise"
+const statisticRoute = require("./routes/statistic");
+const diaryExercises = require("./routes/dairyExercise");
 
 const app = express();
 const swaggerUi = require("swagger-ui-express");
@@ -24,8 +24,8 @@ app.use("/exercises", exercisesRouter);
 app.use("/products", productRouter);
 app.use("/diary/exercises", diaryExercises);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/products", productRouter)
-app.use("/", statisticRoute)
+app.use("/products", productRouter);
+app.use("/", statisticRoute);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const ctrlWrapper = require("./ctrlWrapper");
 const HttpError = require("./HttpError");
 const handleMongooseError = require("./handleMongooseError");
 const dateToShortFormat = require("./dateToShortFormat");
+const emailRegexp = require("./patterns");
 
 module.exports = {
   ctrlWrapper,
   HttpError,
   handleMongooseError,
   dateToShortFormat,
+  emailRegexp,
 };
