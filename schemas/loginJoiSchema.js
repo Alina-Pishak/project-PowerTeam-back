cdf98d7fd1e2148884343c6667585e99a44afb5c
Maxim Sytnik <118841889+Maksim713@users.noreply.github.com>
2024-01-15 16:12:18 +0200

    fix pattern path login and register schemas

--- schemas/loginJoiSchema.js
+++ schemas/loginJoiSchema.js
@@ -1,7 +1,5 @@
 const Joi = require("joi");
-
-// eslint-disable-next-line no-useless-escape
-const emailRegexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const { emailRegexp } = require("../helpers");
 
 const loginJoiSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
